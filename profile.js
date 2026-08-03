330bc811464717f6b9ddca799581cc00ad351692
sophia-leblang <smleblang@gmail.com>
2026-08-02 17:01:18 -0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -11,7 +11,7 @@ If you do not have a link yet, leave that section as an empty list: []
 */
 
 const studentProfile = {
-  name: "Your Name",
+  name: "Sophia Leblang",
 
   tagline: "DREAM-High Scholar | Computational Biology Portfolio",
 
